1682017c97ef395733063f28fd5304ebe5d0b799
kremenovic <kremenovicstefan96@gmail.com>
2021-07-17 18:50:26 -0400

    add new transaction on DOM

--- main.js
+++ main.js
@@ -1,79 +1,154 @@
-// GENERAL
-const jsBalance = document.querySelector(".js-balance");
-const jsTransactions = document.querySelector(".js-transactions");
-const jsProgressBar = document.querySelector(".js-progressBarValue");
-const jsAddIncomeBtn = document.querySelector(".js-addIncome");
-const jsAddExpenseBtn = document.querySelector(".js-addExpense");
-const jsOverlay = document.querySelector(".js-overlay");
-let jsChart = document.getElementById('budgetChart').getContext('2d');
-// POP UP ELEMENTS
-// add income
-const jsAddIncome = document.getElementById("js-addIncome");
-const jsSaveIncome = document.querySelector(".js-saveIncome");
-const jsCancelIncome = document.querySelector(".js-cancelIncome");
-const jsIncomeTitle = document.querySelector(".js-income-title");
-const jsIncomeAmount = document.querySelector(".js-income-amount");
-const jsIncomeCategory = document.querySelector(".js-income-category");
-// add expense
-const jsAddExpense = document.getElementById("js-addExpense");
-const jsSaveExpense = document.querySelector(".js-saveExpense");
-const jsCancelExpense = document.querySelector(".js-cancelExpense");
-const jsExpenseTitle = document.querySelector(".js-expense-title");
-const jsExpenseAmount = document.querySelector(".js-expense-amount");
-const jsExpenseCategory = document.querySelector(".js-expense-category");
-
-// ADD INCOME/EXPENSE POP UP
-const showPopUp = (popup, cancel) => {
-  popup.classList.add("active");
-  jsOverlay.classList.add("active");
-
-  cancel.addEventListener("click", () => {
-    popup.classList.remove("active");
-    jsOverlay.classList.remove("active");
-  })
+// SELECT ELEMENT
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );
 }
 
-// income pop up
-const showAddIncome = () => {
-  showPopUp(jsAddIncome, jsCancelIncome);
+let id = null;
+
+const addIncomeBtn = getElement(".js-btnAddIncome");
+const addExpenseBtn = getElement(".js-btnAddExpense");
+const transactions = getElement(".js-transactions");
+const saveBtn = getElement(".js-save");
+const closeBtn = getElement(".js-cancel");
+
+class Transactions {
+  constructor(element) {
+    this.openBtns = element;
+    this.overlay = getElement(".js-overlay");
+    this.incomeModal = getElement(".js-modal");
+    // bind
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(target) {
+    this.incomeModal.classList.add("active");
+    this.overlay.classList.add("active");
+    this.modalTitle = this.incomeModal.querySelector(".js-modalTitle");
+    this.modalCategories = this.incomeModal.querySelector(".js-category");
+    
+    if(target.classList.contains("js-btnAddIncome")) {
+      this.modalTitle.innerHTML = "Add Income";
+      this.modalCategories.innerHTML = `<option value="Income">Income</option>`;
+
+    } else if (target.classList.contains("js-btnAddExpense")) {
+      this.modalTitle.innerHTML = "Add Expense";
+      this.modalCategories.innerHTML = `<option value="select" disabled selected>Select...</option>
+      <option value="Clothes">Clothes</option>
+      <option value="Gifts">Gifts</option>
+      <option value="Shopping">Shopping</option>
+      <option value="EatingOut">Eating out</option>
+      <option value="Sports">Sports</option>`;
+    }
+  }
+
+  closeModal(target) {
+    target.parentElement.parentElement.classList.remove("active");
+    this.overlay.classList.remove("active");
+  }
+
+  saveTransaction(target) {
+    let modal = target.parentElement.parentElement;
+    let transactionID = id++;
+    this.title = modal.querySelector(".js-title");
+    this.amount = modal.querySelector(".js-amount");
+    this.category = modal.querySelector(".js-category");
+
+    if (this.category.value !== "select") {
+      if (this.title.value !== "" || this.amount.value !== "") {
+        this.createElement(transactionID, this.category.length, this.title.value, this.category.value, this.amount.value);
+        this.closeModal(target);
+        this.backToDefault(this.title, this.amount);
+      } else {
+        alert("You cannot add empty value...");
+      }
+    } else {
+      alert("Please select category...");
+    }
+    
+  }
+
+  createElement(id, categoryNumber, title, category, amount) {
+    let item = document.createElement("li");
+    if(categoryNumber > 1) {
+      item.classList.add("expense");
+
+    } else {
+      item.classList.add("income");
+    }
+
+    item.setAttribute("data-id", id);
+    item.innerHTML = `${category} | ${title}<span class="amount">$${amount}</span>`;
+    transactions.appendChild(item);
+  }
+
+  backToDefault (title, amount) {
+    title.value = "";
+    amount.value = "";
+  }
 }
 
-// expense pop up
-const showAddExpense = () => {
-  showPopUp(jsAddExpense, jsCancelExpense);
-}
+const transaction = new Transactions ();
+const jsChart = getElement("#budgetChart").getContext("2d");
 
 // CHART
 
 // colors
-const chartColors = ["#03DAC5", "#F48FB1", "#9575CD", "#64B5F6", "#DCE775", "#FFB74D"]
-
+const chartColors = [
+  "#03DAC5",
+  "#F48FB1",
+  "#9575CD",
+  "#64B5F6",
+  "#DCE775",
+  "#FFB74D",
+];
 
 const chart = new Chart(jsChart, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: chartColors,
-          borderColor: chartColors,
-          borderWidth: 1
-      }]
+    labels: ["Income", "Clothes", "Gifts", "Shopping", "Eating out", "Sports"],
+    datasets: [
+      {
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: chartColors,
+        borderColor: chartColors,
+        borderWidth: 1,
+      },
+    ],
   },
   options: {
-      plugins: {
-        legend: {
-          display: false
-        }
-      }
-  }
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
 });
 
 // EVENT LISTENERS
-jsAddIncomeBtn.addEventListener("click", showAddIncome);
-jsAddExpenseBtn.addEventListener("click", showAddExpense);
 
 window.addEventListener("DOMContentLoaded", () => {
-  jsChart.canvas.parentNode.style.height = '355px';
-  jsChart.canvas.parentNode.style.width = '355px';
+  jsChart.canvas.parentNode.style.height = "355px";
+  jsChart.canvas.parentNode.style.width = "355px";
+});
+
+addIncomeBtn.addEventListener("click", function (e) {
+  transaction.openModal(e.target);
+})
+
+addExpenseBtn.addEventListener("click", function (e) {
+  transaction.openModal(e.target);
+})
+
+saveBtn.addEventListener("click", function (e) {
+  transaction.saveTransaction(e.target);
+})
+
+closeBtn.addEventListener("click", function (e) {
+  transaction.closeModal(e.target);
 })
